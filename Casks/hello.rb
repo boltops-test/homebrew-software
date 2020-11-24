47cfbbc823b16637a8e568667f73ea9cc8e92fc8
Tung Nguyen <tongueroo@gmail.com>
2020-11-24 08:12:24 -0800

    update package

--- Casks/hello.rb
+++ Casks/hello.rb
@@ -1,13 +1,13 @@
 # This file is generated. To update it, re-run the build process
 cask 'hello' do
   version '0.5.1'
-  sha256 'e2a28695665e56f7306eb66beecb0f0cb4d131c8c229f0a52bc4c280add46c7e'
+  sha256 '67eda8383d88a83a60a62f5ed2716dfd5b1dde75f9fbefd4b6b67fb6ed396c28'
 
-  url "https://tap.tung.boltops.com/hello/0.5.1/hello-0.5.1-20201124155846.dmg"
+  url "https://tap.tung.boltops.com/hello/0.5.1/hello-0.5.1-20201124161158.dmg"
   name 'hello'
   homepage 'https://boltops.com/hello'
 
-  pkg "hello-0.5.1-20201124155846.pkg"
+  pkg "hello-0.5.1-20201124161158.pkg"
 
   uninstall pkgutil: 'test.boltops.pkg.hello'
 end
