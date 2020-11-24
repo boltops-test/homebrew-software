79af43bdd4a5aa75f2523c30b27b6804c1285d69
Tung Nguyen <tongueroo@gmail.com>
2020-11-23 19:04:24 -0800

    test

--- Casks/hello.rb
+++ Casks/hello.rb
@@ -1,11 +1,11 @@
 cask 'hello' do
-  package_name = 'hello-0.5.1-20201122021552.pkg'
+  package_name = 'hello-0.5.1-20201124025947.pkg'
   dmg = package_name.sub('.pkg','.dmg')
 
   version '0.5.1'
-  sha256 '7e03910a8dac2bbe7e50f68a27ee271348a005096214a23ea18e46f57355356d'
+  sha256 '2f0fa3e1fcee31669ddf266f08012f7595b300dd6524b1613ce7f9127f331918'
 
-  url "https://tap2.tung.boltops.com/hello/0.5.1/hello-0.5.1-20201122021552.dmg" # DMG
+  url "https://tap2.tung.boltops.com/hello/0.5.1/hello-0.5.1-20201124025947.dmg" # DMG
   name 'Hello'
   homepage 'https://boltops.com/hello'
 
