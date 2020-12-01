5f4fe2ab28d9afdcd801e7b49405e38cb2248b32
Tung Nguyen <tongueroo@gmail.com>
2020-11-30 17:35:14 -0800

    update hello cask

--- Casks/hello.rb
+++ Casks/hello.rb
@@ -4,9 +4,9 @@ cask 'hello' do
   homepage 'https://boltops.com/hello'
   version '0.5.6'
 
-  pkg "hello-0.5.6-20201201013316.pkg"
-  url "https://tap.tung.boltops.com/hello/0.5.6/hello-0.5.6-20201201013316.dmg"
-  sha256 '19721a2e965398e089a7688b640aed6d63c4c59af67bed530b5253d42460d04c'
+  pkg "hello-0.5.6-20201201013447.pkg"
+  url "https://tap.tung.boltops.com/hello/0.5.6/hello-0.5.6-20201201013447.dmg"
+  sha256 '851a6db2f1bb85ba1fc8b195afe27d13493a30d0b544cedf1920f06a0b1b5f4d'
 
   uninstall pkgutil: 'test.boltops.pkg.hello'
 end
